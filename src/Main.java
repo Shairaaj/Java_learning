c9f5acffa24003657055c6bed5d5c0ae50560cd9
Shairaaj <iamshairaaj@gmail.com>
2025-04-09 13:58:21 +0530

    First commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String [] args) {
+        System.out.println("hi");
+        Hello();
+    }
+    public static void Hello(){
+        System.out.println("Here is the catch");
+    }
+}
