cdf96378c1110fdd539a9b29d48add151c59f483
Shairaaj <iamshairaaj@gmail.com>
2025-04-09 19:52:52 +0530

    completed array

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
-import java.util.Scanner;
 public class Main{
     public static void main(String [] args) {
+        int [] first_arr= {12,2,12,11,19};
+        int[] sec_arr={13,131,12,11};
+        int [][] arr= {first_arr,sec_arr};
 
+        System.out.println();
     }
 }
