4508e898471d837d805247cfa31c6903cc5ca589
Shairaaj <iamshairaaj@gmail.com>
2025-04-09 18:55:06 +0530

    Leveraging Basic Concepts

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
+import java.util.Scanner;
 public class Main{
     public static void main(String [] args) {
-        System.out.println("hi");
-        Hello();
-    }
-    public static void Hello(){
-        System.out.println("Here is the catch");
+
     }
 }
