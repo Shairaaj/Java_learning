a68d8c35d297d6313e03fa84d07635520b99bf93
Shairaaj <iamshairaaj@gmail.com>
2025-04-10 08:53:52 +0530

    Started Strings

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 public class Main{
     public static void main(String [] args) {
-        int [] first_arr= {12,2,12,11,19};
-        int[] sec_arr={13,131,12,11};
-        int [][] arr= {first_arr,sec_arr};
+        String str1= "Rahul";
+        String str2= new String("Rahul");
+        String str3= new String("Rahul");
+        String str4= "Rahul";
 
-        System.out.println();
+        System.out.println(str1==str4);
+        System.out.println(str2==str3);
+        System.out.println(str1==str2);
+        System.out.println(str3.equals(str2));
     }
 }
