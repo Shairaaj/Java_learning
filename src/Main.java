8ff01a49e6401cf7dad25d501a19d61e0e9fe7bb
Shairaaj <iamshairaaj@gmail.com>
2025-04-15 18:48:03 +0530

    Added Constructor

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
-public class Main{
-    public static void main(String [] args) {
-        int[] array ={32,23,234};
-        try{
-            System.out.println(array[2]/0);
-        }catch(Exception expt1){
-            System.out.println(expt1);
-        }
-        System.out.println("hi");
+public class Main {
+   public static void main(String[] args){
+
    }
 }
