4b3d8645a9af0ac55cfc8a343c11021c9931b54f
Shairaaj <iamshairaaj@gmail.com>
2025-04-22 07:17:55 +0530

    Fibonnaci Series

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,22 @@
-public class Main {
-   public static void main(String[] args) {
-      float l=12;
-      float b=12;
-      Rectangle rec2 = new Rectangle();
-      Rectangle rec1 = new Rectangle(l,b);
-      float area= rec1.Area();
-      System.out.println(area);
+import java.util.Scanner;
+
+public class Main{
+   public static void main(String [] args) {
+      Scanner sc= new Scanner(System.in);
+      System.out.println("Enter the number");
+      int num= sc.nextInt();
+
+      int num1=0;
+      int num2=1;
+      int n=0;
+      System.out.println("The number "+(n++)+ " is "+num1);
+      System.out.println("The number "+(n++)+ " is "+num2);
+      int temp=0;
+      for(int i=num-1; i>0; i--){
+         temp = num1 + num2;
+         System.out.println("The number " + (n++) + " is " + temp);
+         num1=num2;
+         num2=temp;
+      }
    }
 }
