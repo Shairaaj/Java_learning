56d91dc35a2b208bda95df23afedda4df9a137d9
Shairaaj <iamshairaaj@gmail.com>
2025-04-22 15:04:27 +0530

    greatest of three numbers

--- src/Main.java
+++ src/Main.java
@@ -4,34 +4,22 @@ public class Main {
       Scanner sc = new Scanner(System.in);
       System.out.println("Enter the number: ");
       int num = sc.nextInt();
-      String st= Integer.toString(num);
-      System.out.println(st);
-      boolean isNegative=st.startsWith("-");
-      String str= isNegative?st.substring(1):st;
-      ArrayList<Integer> arr= new ArrayList<>();
-      for (int i=0; i<str.length(); i++){
-         int temp=0;
-         temp=str.charAt(i)-'0';
-         arr.add(temp);
-      }
-      System.out.println(arr);
 
-      int number= reverseNumber(arr);
-      if(isNegative){
-         number=number*-1;
+      String st= Integer.toString(num);
+      int [] arr= new int[st.length()];
+      for (int i=0;i<arr.length; i++){
+         arr[i]=st.charAt(i)-'0';
       }
-      System.out.println("The reversed number for "+num+ " is "+number);
+      int greatest= greatestNum(arr);
+      System.out.println("The greatest number is: "+greatest);
    }
-   public static int reverseNumber(ArrayList<Integer> arr){
-      ArrayList <Integer>array= new ArrayList<>();
-      for(int i= arr.size()-1; i>=0;i--) {
-         array.add(arr.get(i));
+   public static int greatestNum(int[] arr){
+      int greatest =arr[0];
+      for(int digit: arr){
+         if(digit>=greatest){
+            greatest=digit;
+         }
       }
-      System.out.println(array);
-      int num=0;
-      for (int digit:array){
-         num=num*10+digit;
-      }
-      return num;
+      return greatest;
    }
 }
