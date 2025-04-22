305464f8d3d16d1023baa70fee6f70d2112516a7
Shairaaj <iamshairaaj@gmail.com>
2025-04-22 19:59:57 +0530

    Anagram which checks for spaces and uppercases

--- src/Main.java
+++ src/Main.java
@@ -2,37 +2,34 @@ import java.util.*;
 public class Main {
    public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
-      System.out.println("Enter the string 1: ");
-      String str1= sc.nextLine();
-      System.out.println("Enter the string 2:");
-      String str2= sc.nextLine();
-
-      char [] arr1= new char[str1.length()];
-      char [] arr2= new char[str2.length()];
-
-      for (int i=0; i<str1.length(); i++){
-         arr1[i]=str1.charAt(i);
+      System.out.println("Enter the string : ");
+      String st = sc.nextLine();
+      String str=st.toLowerCase();
+      ArrayList<Character> arr= new ArrayList<>();
+      for(int i=0; i<str.length(); i++){
+         char temp;
+         temp=str.charAt(i);
+         arr.add(temp);
       }
-      for (int j=0; j<str2.length(); j++){
-         arr2[j]=str2.charAt(j);
-      }
-      System.out.println(Arrays.toString(arr1)+"\n"+Arrays.toString(arr2));
-      System.out.println(isAnagram(arr1,arr2));
+      ArrayList<Integer> result= new ArrayList<>();
+      result= countVowCon(arr);
+      System.out.println(result);
    }
-   public static boolean isAnagram(char[] arr1, char[] arr2){
-      ArrayList<Character> array= new ArrayList<>();
-      outer:
-      for (int i=0; i< arr1.length; i++){
-         for(int j=0; j<arr2.length; j++){
-            if(arr1[i]==arr2[j]){
-               array.add(arr1[i]);
-               continue outer;
-            }
+   public static ArrayList<Integer> countVowCon(ArrayList<Character> arr){
+      ArrayList<Integer> count= new ArrayList<>();
+      int countVow=0;
+      int countCon=0;
+      for (int i=0; i<arr.size(); i++){
+         if(arr.get(i)==' ') continue;
+         if(arr.get(i)=='a' || arr.get(i)=='e'|| arr.get(i)=='i'|| arr.get(i)=='o' || arr.get(i)=='u'){
+            ++countVow;
+         }
+         else{
+            ++countCon;
          }
       }
-      if(array.size()==arr1.length){
-         return true;
-      }
-      return false;
+      count.add(countVow);
+      count.add(countCon);
+      return count;
    }
 }
