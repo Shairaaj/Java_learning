b423bd027015a76c84c8bb0a98d1e1315490da34
Shairaaj <iamshairaaj@gmail.com>
2025-04-22 18:43:14 +0530

    Anagram

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,37 @@ import java.util.*;
 public class Main {
    public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
-      System.out.println("Enter the number: ");
-      int num = sc.nextInt();
-      ArrayList <Integer> array= new ArrayList<>();
-      array=checkPrime(num);
-      System.out.println(array);
+      System.out.println("Enter the string 1: ");
+      String str1= sc.nextLine();
+      System.out.println("Enter the string 2:");
+      String str2= sc.nextLine();
+
+      char [] arr1= new char[str1.length()];
+      char [] arr2= new char[str2.length()];
+
+      for (int i=0; i<str1.length(); i++){
+         arr1[i]=str1.charAt(i);
+      }
+      for (int j=0; j<str2.length(); j++){
+         arr2[j]=str2.charAt(j);
+      }
+      System.out.println(Arrays.toString(arr1)+"\n"+Arrays.toString(arr2));
+      System.out.println(isAnagram(arr1,arr2));
    }
-   public static ArrayList<Integer> checkPrime(int num){
-      ArrayList <Integer> arr= new ArrayList<>();
-      int j=0;
-      for(int i=2; i<=num; ++i){
-         if(i==2 || i==3 || i==5 || i==7){
-            continue;
-         }
-         if(i%2==0 || i%3==0 || i%5==0 || i%7==0){
-            continue;
-         }
-         else{
-            arr.add(i);
-            ++j;
+   public static boolean isAnagram(char[] arr1, char[] arr2){
+      ArrayList<Character> array= new ArrayList<>();
+      outer:
+      for (int i=0; i< arr1.length; i++){
+         for(int j=0; j<arr2.length; j++){
+            if(arr1[i]==arr2[j]){
+               array.add(arr1[i]);
+               continue outer;
+            }
          }
       }
-      return arr;
+      if(array.size()==arr1.length){
+         return true;
+      }
+      return false;
    }
 }
