8734d2e6a9544e0a331ae877671cb01a775c00c0
Shairaaj <iamshairaaj@gmail.com>
2025-04-23 14:39:45 +0530

    The biggest and smallest in an array

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,23 @@ import java.util.*;
 public class Main {
    public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
-      System.out.println("Enter the sentence : ");
-      String st = sc.nextLine();
-      String stUp= st.toUpperCase();
-      String str= stUp.replaceAll("[. ;:,]","");
-      System.out.println(str);
-      System.out.println(checkPalindrome(str));
-   }
-   public static boolean checkPalindrome(String str){
-      for (int i=0; i<str.length()/2; i++){
-         if(str.charAt(i)!=str.charAt(str.length()-1-i)){
-            return false;
-         }
-      }return true;
-   }
+      int [] array= new int [10];
+      for (int i=0; i<array.length;i++){
+          System.out.print("Enter the number:");
+          array[i]=sc.nextInt();
+      }
+       System.out.println(Arrays.toString(array));
+      int big=0;
+      int small=array[0];
+      for (int i=0; i<array.length;i++){
+          if(big<=array[i]){
+              big=array[i];
+          }
+          if(array[i]<small){
+              small=array[i];
+          }
+      }
+       System.out.println("The biggest number is: "+big);
+       System.out.println("The smallest number is: "+small);
+      }
 }
