395258708b7182944d11a3f74c6f559ee602ec48
Shairaaj <iamshairaaj@gmail.com>
2025-04-23 07:24:46 +0530

    palidrome

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,18 @@ import java.util.*;
 public class Main {
    public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
-      System.out.println("Enter the word : ");
-      char st = sc.next().charAt(0);
-      System.out.println((int)st);
+      System.out.println("Enter the sentence : ");
+      String st = sc.nextLine();
+      String stUp= st.toUpperCase();
+      String str= stUp.replaceAll("[. ;:,]","");
+      System.out.println(str);
+      System.out.println(checkPalindrome(str));
+   }
+   public static boolean checkPalindrome(String str){
+      for (int i=0; i<str.length()/2; i++){
+         if(str.charAt(i)!=str.charAt(str.length()-1-i)){
+            return false;
+         }
+      }return true;
    }
 }
