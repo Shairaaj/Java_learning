40c6d9aa9fc170e3ae209aaf85c3b5e94bb0a3e0
Shairaaj <iamshairaaj@gmail.com>
2025-04-23 07:06:05 +0530

    To find the ASCII value of a word

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,8 @@ import java.util.*;
 public class Main {
    public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
-      System.out.println("Enter the sentence : ");
-      String st = sc.nextLine();
-      String sent= st.toUpperCase();
-      String sentence= sent.replaceAll("[ .;]","");
-      int size=sentence.length();
-      System.out.println(checkPangram(sentence,size));
-
-   }
-   public static boolean checkPangram(String sentence,int size){
-      ArrayList<Character> arr= new ArrayList<>();
-      for (int i=0; i<sentence.length();i++){
-         int digit= sentence.charAt(i);
-         for (int j=65; j<=90; j++){
-            if(digit==j){
-               arr.add(sentence.charAt(i));
-            }
-         }
-      }
-      return arr.size()==size;
-
+      System.out.println("Enter the word : ");
+      char st = sc.next().charAt(0);
+      System.out.println((int)st);
    }
 }
