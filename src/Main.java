4d5c815205a64f84bae18646abc55ebffb386f28
Shairaaj <iamshairaaj@gmail.com>
2025-04-23 07:03:35 +0530

    Pangram with ASCII that checks if all the albhabets are present in a sentence

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,25 @@ import java.util.*;
 public class Main {
    public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
-      System.out.println("Enter the value1 : ");
-      int num1 = sc.nextInt();
-      System.out.println("Enter the value2 : ");
-      int num2= sc.nextInt();
+      System.out.println("Enter the sentence : ");
+      String st = sc.nextLine();
+      String sent= st.toUpperCase();
+      String sentence= sent.replaceAll("[ .;]","");
+      int size=sentence.length();
+      System.out.println(checkPangram(sentence,size));
 
-      System.out.println(num1+" "+num2);
-      // 10    ,     7
-      num2= num1+num2; //17
-      num1=num2-num1;  //7
-      num2= num2-num1;
+   }
+   public static boolean checkPangram(String sentence,int size){
+      ArrayList<Character> arr= new ArrayList<>();
+      for (int i=0; i<sentence.length();i++){
+         int digit= sentence.charAt(i);
+         for (int j=65; j<=90; j++){
+            if(digit==j){
+               arr.add(sentence.charAt(i));
+            }
+         }
+      }
+      return arr.size()==size;
 
-
-
-
-      System.out.println(num1+" "+num2);
    }
 }
