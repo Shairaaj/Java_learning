dcc9d7540c826986f5cd0541535f734446676d64
Shairaaj <iamshairaaj@gmail.com>
2025-05-02 07:06:43 +0530

    leetcode 2180

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,18 @@
 import java.util.*;
 public class Main {
    public static void main(String[] args) {
-       String s="leetcode";
-       StringBuilder sb= new StringBuilder();
-       HashMap<Character,Integer> alpha= new HashMap<>();
-       for(char i = 'a'; i<='z'; i++){
-           alpha.put(i,i-'a'+1);
-       }
-       for(int i=0; i<s.length();i++){
-           char c;
-           c=s.charAt(i);
-           sb.append(alpha.get(c));
-       }
-       System.out.println(sb);
-       int k=0;
-       int sum=0;
-       while(k<2) {
-           sum=0;
-           int num=0;
-           for (int i = 0; i < sb.length(); i++) {
-               num = sb.charAt(i)-'0';
-               sum += num;
+       int num=30;
+       int count=0;
+       for(int i=1; i<=num; i++){
+           String n=Integer.toString(i);
+           int sum=0;
+           for(int j=0;j<n.length();j++){
+               sum+=n.charAt(j)-'0';
            }
-           sb = new StringBuilder();
-           System.out.println(sum);
-           sb.append(sum);
-           k++;
+           if(sum%2==0){
+               ++count;
+           }
        }
-       System.out.println(sum);
+       System.out.println(count);
    }
 }
