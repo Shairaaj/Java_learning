461b2d26f2df4368ba2aeab7337412faab9e0954
Shairaaj <iamshairaaj@gmail.com>
2025-05-15 23:13:18 +0530

    Word Pattern

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,33 @@
+import java.sql.SQLOutput;
 import java.util.*;
 public class Main {
    public static void main(String[] args){
-      String[] str= new String[]{"neat","code","love","you"};
-      String convStr= encode(str);
-      System.out.println(convStr);
-      String[] deStr= decode(convStr,str.length);
-   }
-   public static String encode(String[] str){
-      StringBuilder stb= new StringBuilder();
-      for(String s: str){
-         int n= s.length();
-         stb.append(n);
-         stb.append(s);
-         stb.append('#');
-      }
-      return new String(stb);
-   }
-   public static String[] decode(String str,int n){
-      String[] res= new String[n];
-      System.out.println(str.length());
-      int pos=0; int i=0; int skip= str.charAt(pos)-'0'; int range=0; range=skip;
-      while(range<str.length()) {
-         if (str.charAt(range + 1) == '#') {
-            String subStr = str.substring(pos + 1, range+1);
-            res[i] = subStr;
-            System.out.println(Arrays.toString(res));
-            i++;
-         }
-         if(i<n){
-         pos=range+2;
-         System.out.println("First value:"+pos);
-         skip = str.charAt(range+2)-'0';
-         System.out.println("Skip value: "+skip);
-         range=range+skip+2;
-      }
-         else{
-            break;
-         }
-      }
-      return res;
+       String s= "dog cat dog cat";
+       String[] str= s.split(" ");
+      System.out.println(Arrays.toString(str));
+       String pattern= "abba";
+       pattern=pattern.strip();
+       LinkedHashSet<Character> sP= new LinkedHashSet<>();
+       
+       for(Character c: pattern.toCharArray()){
+          sP.add(c);
+       }
+       LinkedHashSet<String> sS = new LinkedHashSet<>(Arrays.asList(str));
+
+       LinkedHashMap<String,Character> linkedHashMap= new LinkedHashMap<>();
+       Iterator<Character> i1= sP.iterator();
+       Iterator<String> i2= sS.iterator();
+
+       while(i1.hasNext() && i2.hasNext()){
+          linkedHashMap.put(i2.next(),i1.next());
+       }
+      System.out.println(linkedHashMap);
+       int i=0;
+       for(String st: str){
+          if(linkedHashMap.get(st)!=pattern.charAt(i)){
+             System.out.println("false");
+          }
+          i++;
+       }
    }
 }
